e06d70fb95d8f5e84b6572a260a71ff4b414a810
Haldor201 <calderonhaldor385@gmail.com>
2024-10-04 10:22:45 -0500

    server host

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-naming: python -m Pyro4.naming
+naming: python -m Pyro4.naming --host 0.0.0.0 --port 9090
 server: python server.py
--- client.py
+++ client.py
@@ -1,11 +1,24 @@
-# saved as greeting-client.py
+# saved as client.py
 import Pyro4
 import sys
+
 try:
-    num = int(input("write a int number "))
-except:
-    print("ingresa un numero valido")
+    num = int(input("Escribe un número entero: "))
+except ValueError:
+    print("Ingresa un número válido.")
     sys.exit()
 
-getFactClass = Pyro4.Proxy("PYRONAME:example.fact")    # use name server object lookup uri shortcut
-print(getFactClass.get_facto(num))
+# Configurar el nombre del servidor
+Pyro4.config.NS_HOST = "pyro-euq6.onrender.com"  # Cambia esto por la URL de tu naming server
+Pyro4.config.NS_PORT = 9090  # Asegúrate de que este puerto sea el correcto
+
+try:
+    getFactClass = Pyro4.Proxy("PYRONAME:example.fact")  # Buscar el objeto remoto
+    print(getFactClass.get_facto(num))
+except Pyro4.errors.CommunicationError as e:
+    print(f"Error de comunicación: {e}")
+except Pyro4.errors.NamingError as e:
+    print(f"Error de nombres: {e}")
+except Exception as e:
+    print(f"Se produjo un error: {e}")
+
